4956c2438b8e232ff74e6fa6893fafdcc36fa147
Xandra Bello <alexandrabello0130@gmail.com>
2018-03-19 13:49:11 +0800

    models

---
+++ application/models/client_qoute_model.php
@@ -0,0 +1,41 @@
+<?php
+
+class Client_model extends CI_Model {
+    private $table = 'client_qoute';
+    
+    function create($client_qouteRecord){
+        $this->db->insert($this->table, $client_qouteRecord);
+    }
+    
+    function read($condition=null){
+        $this->db->select('*');
+        $this->db->from($this->table);
+        
+        if(isset($condition))
+            $this->db->where($condition);
+        
+        $query = $this->db->get();
+        
+        if($query->num_rows()>0)
+            return $query->result_array();
+        else
+            return false;
+        
+    }
+    
+    function update($newRecord){
+        $this->db->replace($this->table,$newRecord);
+    }
+    
+    function del($where_array){
+        $this->db->delete($this->table,$where_array);
+    }
+
+    function count(){
+    $this->db->select('*');
+    $this->db->from($this->table);
+    $query = $this->db->get();
+    return $query->num_rows();
+    }
+}
+?>
